699b59bba1dbe4233bdc5642250460a129dacd39
Smruti Sahoo <smruti.sahoo@siemens.com>
2019-07-25 13:21:23 +0530

    fix(license-todos): Fixed the bug w.r.t adding todos to license
    Signed-off-by: Smruti Sahoo <smruti.sahoo@siemens.com>

--- frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/licenses/LicensesPortlet.java
+++ frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/licenses/LicensesPortlet.java
@@ -31,6 +31,7 @@ import org.eclipse.sw360.portal.common.UsedAsLiferayAction;
 import org.eclipse.sw360.portal.portlets.Sw360Portlet;
 import org.eclipse.sw360.portal.users.UserCacheHolder;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.thrift.TException;
 
@@ -403,6 +404,7 @@ public class LicensesPortlet extends Sw360Portlet {
             todo.setObligationDatabaseIds(Collections.emptySet());
         }
         todo.setText(todoText);
+        todo.setTitle(StringUtils.EMPTY);
 
         User user = UserCacheHolder.getUserFromRequest(request);
         String moderationComment = request.getParameter(PortalConstants.MODERATION_REQUEST_COMMENT);
