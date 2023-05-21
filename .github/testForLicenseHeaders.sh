f1899425294c08aebad841bb06b00bdc01b940e5
Anupam Ghosh <ag.4ums@gmail.com>
2023-05-21 16:21:14 +0530

    Revert "feat(libs): Add couchdb-lucene as third party"
    This reverts commit 53236b5903b094cdb3349619537a5ba19524ab3d.

--- .github/testForLicenseHeaders.sh
+++ .github/testForLicenseHeaders.sh
@@ -39,7 +39,6 @@ done <<< "$(git ls-files \
     | grep -v 'id_rsa' \
     | grep -v '.versions' \
     | grep -v 'default_secrets' \
-    | grep -Ev 'third-party/couchdb-lucene/*' \
     | grep -Ev '*/asciidoc/*')"
 
 if [ "$failure" = true ]; then
