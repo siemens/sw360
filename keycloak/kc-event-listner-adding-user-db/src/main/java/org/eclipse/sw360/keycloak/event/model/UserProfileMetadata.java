a2efd6cfcbc755d4f0afa5013d720487f5469c4f
Smruti Prakash Sahoo <smruti.sahoo@siemens.com>
2024-04-11 20:40:00 +0530

    feat(keycloak): Added keycloak libraries

---
+++ keycloak/kc-event-listner-adding-user-db/src/main/java/org/eclipse/sw360/keycloak/event/model/UserProfileMetadata.java
@@ -0,0 +1,24 @@
+package org.eclipse.sw360.keycloak.event.model;
+
+import java.util.List;
+
+public class UserProfileMetadata {
+	private List<Attribute> attributes;
+	private List<String> groups;
+
+	public List<Attribute> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<Attribute> attributes) {
+		this.attributes = attributes;
+	}
+
+	public List<String> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<String> groups) {
+		this.groups = groups;
+	}
+}
---
+++ keycloak/sw360-keycloak-user-storage-provider/src/main/java/org/eclipse/sw360/keycloak/storage/user/Sw360UserStorageProvider.java
@@ -0,0 +1,223 @@
+package org.eclipse.sw360.keycloak.storage.user;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.eclipse.sw360.datahandler.thrift.users.User;
+import org.jboss.logging.Logger;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.CredentialInputUpdater;
+import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.models.GroupModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
+import org.keycloak.models.UserCredentialModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.cache.CachedUserModel;
+import org.keycloak.models.cache.OnUserCache;
+import org.keycloak.models.credential.PasswordCredentialModel;
+import org.keycloak.storage.StorageId;
+import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.user.UserLookupProvider;
+import org.keycloak.storage.user.UserQueryProvider;
+import org.keycloak.storage.user.UserRegistrationProvider;
+
+public class Sw360UserStorageProvider implements UserStorageProvider,
+        UserLookupProvider,
+        UserRegistrationProvider,
+        UserQueryProvider,
+        CredentialInputUpdater,
+        CredentialInputValidator,
+        OnUserCache
+{
+    private static final Logger logger = Logger.getLogger(Sw360UserStorageProvider.class);
+    public static final String PASSWORD_CACHE_KEY = UserAdapter.class.getName() + ".password";
+
+    protected ComponentModel model;
+    protected KeycloakSession session;
+    Sw360UserService sw360UserService;
+
+    Sw360UserStorageProvider(KeycloakSession session, ComponentModel model) {
+        this.session = session;
+        this.model = model;
+        sw360UserService = new Sw360UserService();
+    }
+
+    @Override
+    public void preRemove(RealmModel realm) {
+
+    }
+
+    @Override
+    public void preRemove(RealmModel realm, GroupModel group) {
+
+    }
+
+    @Override
+    public void preRemove(RealmModel realm, RoleModel role) {
+
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public UserModel getUserById(RealmModel realm, String id) {
+        logger.info("getUserById: " + id);
+        String persistenceId = StorageId.externalId(id);
+        logger.info("getUserById: ExternalId(CouchDB Id)" + persistenceId);
+        User user = sw360UserService.getUser(persistenceId);
+        logger.info("getUserById: User" + user);
+        if (user == null) {
+            logger.info("could not find user by id: " + id);
+            return null;
+        }
+        return new UserAdapter(session, realm, model, user);
+    }
+
+    @Override
+    public UserModel getUserByUsername(RealmModel realm, String username) {
+        logger.info("getUserByUsername: " + username);
+        User user = null;
+        try {
+          user = sw360UserService.getUserByEmailOrExternalId(username);
+        if (user == null) {
+            logger.info("could not find username: " + username);
+            return null;
+        }
+        } catch(Exception ex) {
+        	return null;
+        }
+
+        return new UserAdapter(session, realm, model, user);
+    }
+
+    @Override
+    public UserModel getUserByEmail(RealmModel realm, String email) {
+		User user = null;
+		try {
+			user = sw360UserService.getUserByEmailOrExternalId(email);
+			if (user == null) {
+				logger.info("could not find email: " + email);
+				return null;
+			}
+		} catch (Exception ex) {
+			return null;
+		}
+        return new UserAdapter(session, realm, model, user);
+    }
+
+    @Override
+    public UserModel addUser(RealmModel realm, String username) {
+        User entity = new User();
+        entity.setDepartment("Dummy");
+        entity.setEmail(username);
+        User user = sw360UserService.addUser(entity);
+        logger.info("added user: " + username);
+        return new UserAdapter(session, realm, model, user);
+    }
+
+    @Override
+    public boolean removeUser(RealmModel realm, UserModel user) {
+        return true;
+    }
+
+    @Override
+    public void onCache(RealmModel realm, CachedUserModel user, UserModel delegate) {
+        String password = ((UserAdapter)delegate).getPassword();
+        if (password != null) {
+            user.getCachedWith().put(PASSWORD_CACHE_KEY, password);
+        }
+    }
+
+    @Override
+    public boolean supportsCredentialType(String credentialType) {
+        return PasswordCredentialModel.TYPE.equals(credentialType);
+    }
+
+    @Override
+    public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
+        if (!supportsCredentialType(input.getType()) || !(input instanceof UserCredentialModel)) return false;
+        UserCredentialModel cred = (UserCredentialModel)input;
+        UserAdapter adapter = getUserAdapter(user);
+        adapter.setPassword(cred.getValue());
+
+        return true;
+    }
+
+    public UserAdapter getUserAdapter(UserModel user) {
+        if (user instanceof CachedUserModel) {
+            return (UserAdapter)((CachedUserModel) user).getDelegateForUpdate();
+        } else {
+            return (UserAdapter) user;
+        }
+    }
+
+    @Override
+    public void disableCredentialType(RealmModel realm, UserModel user, String credentialType) {
+        if (!supportsCredentialType(credentialType)) return;
+
+        getUserAdapter(user).setPassword(null);
+
+    }
+
+    @Override
+    public Stream<String> getDisableableCredentialTypesStream(RealmModel realm, UserModel user) {
+        if (getUserAdapter(user).getPassword() != null) {
+            Set<String> set = new HashSet<>();
+            set.add(PasswordCredentialModel.TYPE);
+            return set.stream();
+        } else {
+            return Stream.empty();
+        }
+    }
+
+    @Override
+    public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
+        return supportsCredentialType(credentialType) && getPassword(user) != null;
+    }
+
+    @Override
+    public boolean isValid(RealmModel realm, UserModel user, CredentialInput input) {
+        if (!supportsCredentialType(input.getType()) || !(input instanceof UserCredentialModel)) return false;
+        UserCredentialModel cred = (UserCredentialModel)input;
+        String password = getPassword(user);
+        return password != null && password.equals(cred.getValue());
+    }
+
+    public String getPassword(UserModel user) {
+        String password = null;
+        if (user instanceof CachedUserModel) {
+            password = (String)((CachedUserModel)user).getCachedWith().get(PASSWORD_CACHE_KEY);
+        } else if (user instanceof UserAdapter) {
+            password = ((UserAdapter)user).getPassword();
+        }
+        return password;
+    }
+
+    @Override
+    public int getUsersCount(RealmModel realm) {
+    	int count = sw360UserService.getAllUsers().size();
+        return count;
+    }
+
+    @Override
+    public Stream<UserModel> searchForUserStream(RealmModel realm, Map<String, String> params, Integer firstResult, Integer maxResults) {
+    	return Stream.empty();
+    }
+
+    @Override
+    public Stream<UserModel> getGroupMembersStream(RealmModel realm, GroupModel group, Integer firstResult, Integer maxResults) {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<UserModel> searchForUserByUserAttributeStream(RealmModel realm, String attrName, String attrValue) {
+        return Stream.empty();
+    }
+}
